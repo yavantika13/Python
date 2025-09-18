db93793daacd38fbff3596ffaf3dddd2bcbab1b9
yavantika13 <yavantika165@gmail.com>
2025-09-18 14:50:51 +0530

    hello

---
+++ new.py
@@ -0,0 +1,6 @@
+# fruits =["apple","banana","mango","grapes"]
+# for fruit in fruits:
+#     print(fruit)
+
+for i in range(1,11):
+    print(f"2 X {i} = {2 * i}")
---
+++ num.py
@@ -0,0 +1,5 @@
+num = 123
+total =0
+for digits in str(num):
+    total += int(digits)
+    print("sum of digits:",total)
