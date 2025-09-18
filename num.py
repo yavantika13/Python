f7343d6bad59840279df6a0fceed71ddcfd6ba90
yavantika13 <yavantika165@gmail.com>
2025-09-18 15:06:44 +0530

    hello

--- num.py
+++ num.py
@@ -1,5 +1,11 @@
-num = 123
-total =0
-for digits in str(num):
-    total += int(digits)
-    print("sum of digits:",total)
+# num = 123
+# total =0
+# for digits in str(num):
+#     total += int(digits)
+#     print("sum of digits:",total)
+    # reverse a string
+text ="my name is isha"
+reversed=" "
+for ch in text:
+        reversed = ch + reversed 
+        print("reversed:",reversed)
