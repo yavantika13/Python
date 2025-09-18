8c1b8649332b45e8049a9a6a12e7e48c5251f083
yavantika13 <yavantika165@gmail.com>
2025-09-18 15:17:42 +0530

    hello

--- fact.py
+++ fact.py
@@ -1,5 +1,18 @@
-n=5
-fact = 1
-for i in range(1, n+1):
-    fact*=i
-print("factorial:",fact)    
+# n=5
+# fact = 1
+# for i in range(1, n+1):
+#     fact*=i
+# print("factorial:",fact)  
+
+
+# counts vowels and consonants
+text ="in this code we will count vowels and consonents "  
+vowels="aeiouAEIOU"
+v_count = 0
+c_count = 0
+for ch in text:
+    if ch.isalpha()  :
+     v_count+=1
+    else:
+     c_count+=1
+     print("vowels:",v_count,"consonents:",c_count)
